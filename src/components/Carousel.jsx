c28c3b2fd265358f997b7456d08d7bb4b3d55ac8
lohi212 <suvvarilohita@gmail.com>
2023-04-18 20:35:21 +0530

    feat(transform): transform translate carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,12 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Carousel = ({ images }) => {
+  const [curr, setCurr] = useState(0);
+  const slides = document.querySelectorAll(".slide");
+
+  const handleNextClick = () => {
+    console.log("===");
+    let newCurr = 0;
+    if (curr === slides.length - 1) newCurr = 0;
+    else newCurr = curr + 1;
+
+    slides.forEach((slide, indx) => {
+      slide.style.transform = `translateX(${400 * (indx - newCurr)}px)`;
+    });
+    setCurr(newCurr);
+  };
+
+  const handlePrevClick = () => {
+    let newCurr = 0;
+    if (curr === 0) newCurr = slides.length - 1;
+    else newCurr = curr - 1;
+
+    slides.forEach((slide, indx) => {
+      slide.style.transform = `translateX(${400 * (indx - newCurr)}px)`;
+    });
+    setCurr(newCurr);
+  };
+
   return (
     <div className="carousel-container">
-      <button className="prev-button button-container">prev</button>
-      <button className="next-button button-container">next</button>
-      {images.map((image) => (
-        <div className="slide">
+      <button
+        className="prev-button button-container"
+        onClick={handlePrevClick}
+      >
+        prev
+      </button>
+      <button
+        className="next-button button-container"
+        onClick={handleNextClick}
+      >
+        next
+      </button>
+      {images.map((image, idx) => (
+        <div
+          className="slide"
+          key={image}
+          style={{
+            transform: `translateX(${idx * 400}px)`,
+          }}
+        >
           <img
             src={image}
             width={400}
